bc5150c4a03d7156f423605fe4bd4461c5f20380
Julmer Olivero Ocando <jolivero.03@gmail.com>
2014-06-26 05:19:36 -0430

    Revert "Puesta a Punto de Plantillas de Correo"
    This reverts commit d9984172ee89ee5e9a3b00ae0c575315c3180d10.

--- app/views/emails/factura.blade.php
+++ app/views/emails/factura.blade.php
@@ -10,11 +10,11 @@
 
 </head>
 
-<body style="font-family: sans-serif; background-color: #f2f2f2; margin: 0; color: #333;"> 
+<body style="font-family: sans-serif; background-color: #353637; margin: 0; color: #ecf0f1;"> 
 
-	<div style="width: 100%; background-color: #C74751; padding: 0.3em; text-align: center; border-bottom: 3px solid #8D2C33;">
+	<div style="width: 100%; background-color: #de2824; padding: 1em; text-align: center; border-bottom: 5px solid #c0392b;">
 
-		<img src="http://pioggia.craftersama.me/assets/images/logo.png" alt="">
+		<img src="http://joelblackberryzone.com.ve/img/logo.png" alt="">
 
 	</div>
 
@@ -22,7 +22,7 @@
 
 	<div style="padding: 0 1em;"> 
 
-		<h1>Recibo No. {{ $datos['id'] }} </h1>
+		<h1>Factura N# {{ $datos['id'] }} </h1>
 
 		<hr>
 
@@ -52,13 +52,13 @@
 
 			<tr>
 
-				<th style="background-color: #C74751;">Producto</th>
+				<th style="background-color: black;">Producto</th>
 
-				<th style="background-color: #C74751;">Cantidad</th>
+				<th style="background-color: black;">Cantidad</th>
 
-				<th style="background-color: #C74751;">Precio</th>
+				<th style="background-color: black;">Precio</th>
 
-				<th style="background-color: #C74751;">Subtotal</th>
+				<th style="background-color: black;">Subtotal</th>
 
 			</tr>
 
@@ -92,20 +92,25 @@
 
 		<div align="center">
         
-        <div style="background-color:#CCC; border-radius:5px; width:80%; color:#000; margin-bottom:50px;">
-        <span style="font-weight:bold; font-size:20px; color:#C74751;">Instrucciones Para Completar tu Compra<br></span>
+        <div style="background-color:#CCC; width:80%; color:#000; margin-bottom:50px;">
+        <span style="font-weight:bold; font-size:20px; color:#F00;">Instrucciones Para Completar tu Compra<br></span>
         <span style="font-weight:bold;">Deposita o has una transferencia a las siguientes Cuentas.<br><br></span>
         
-        <span>Banesco Banco Universal <strong style="color:#060;">Banesco</strong><br>
-        Cuenta Corriente No. <strong>0134-0000-00-0000000000</strong><br>
-        A Nombre de: <strong>Vivian Medina</strong><br>
-        C.I. No. <strong>14738935</strong><br><br></span>
-      
+        <span>Banco Occidental de Descuento <strong style="color:#060;">B.O.D.</strong><br>
+        Cuenta Corriente No. <strong>0116-0062-07-0015733190</strong><br>
+        A nombre de: <strong>Joel Oquendo</strong><br>
+        C.I. No. <strong>18575744</strong><br><br></span>
+        
+        <span>Banco <strong style="color:#F00;">BICENTENARIO</strong><br>
+        Cuenta Corriente No. <strong>0175-0241-70-0071874321</strong><br>
+        A nombre de: <strong>JoelBlackberryZone C.A.</strong><br>
+        RIF. <strong>J-40213263-8</strong><br><br><br></span>
+        
         
         LUEGO REPORTA TU PAGO HACIENDO CLICK EL EL SIGUIENTE LINK "COMPLETAR PAGO", LLENA EL FORMILARIO Y LISTO.
         </div>
 
-			<a href="http://pioggia.craftersama.me/factura/{{ $datos['slug'] }}" style="font-size: 28px; background-color: #C74751; color: #ecf0f1; text-decoration: none; padding: .2em; border-bottom: 3px solid #8D2C33; margin: 0 auto; border-radius: 5px"> Completar Pago </a>
+			<a href="http://joelblackberryzone.com.ve/factura/{{ $datos['slug'] }}" style="font-size: 28px; background-color: #de2824; color: #ecf0f1; text-decoration: none; padding: .6em; border-bottom: 5px solid #c0392b; margin: 0 auto;"> Completar Pago </a>
 
 		</div>
 
