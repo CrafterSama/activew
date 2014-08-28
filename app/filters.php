3356bf38ecb7416a5ea195bfb1b42056ba85f05f
Eli José Carrasquero [PC] <ielijose@gmail.com>
2014-08-28 14:44:28 -0430

    Filter

--- app/filters.php
+++ app/filters.php
@@ -35,7 +35,7 @@ App::after(function($request, $response)
 
 Route::filter('auth', function($route, $request)
 {
-	if (Auth::guest()) return Redirect::guest('login?redirect=' . Request::path()  );
+	if (Auth::guest()) return Redirect::guest('login?redirect=' . Request::fullUrl()  );
 });
 
 
